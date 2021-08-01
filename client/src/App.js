893bdb689529cc9403358e64ec48b1834bd2c697
Kevin Franklin <kevinfranklin59@gmail.com>
2021-08-01 00:17:36 -0400

    new modules

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'  //arrow function... type in rfce. creates it for you!
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {    //run it by typing in console cd ./client/
+                    //then npm start
+    return (
+        <div>
+            
+        </div>
+    )
 }
 
-export default App;
+export default App
